8ffce3b8b40e405147f247bf6310bdb0128b4ae9
十雪拾雪 <nineto0@163.com>
2023-01-20 21:54:22 +0800

    Create hookcontrol.js

---
+++ frame/hookcontrol.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+'use strict';
+//hook后返回hookId失败返回-1
+//初始化应该初始化hook表
+function hookInit(){}
+function setHook(pluginName,eventName,hookType,targetApi,priority){}
+function unsetHook(hookId){}
+
