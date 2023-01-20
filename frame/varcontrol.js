d3ecab68d5abdaeebce3727845b012201bb371bb
十雪拾雪 <nineto0@163.com>
2023-01-20 21:48:13 +0800

    Create varcontrol.js

---
+++ frame/varcontrol.js
@@ -0,0 +1,7 @@
+//变量控制 须知防止越权读取分应用隔离变量 同时公开部分
+function initVarControl(){}
+function setAuth(token,address){}
+function getAuth(token,address){}
+function readVar(token,address){}
+function writeVar(token,address){}
+//需要实现的
