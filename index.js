d1b8fbe421681b5781f4cd46fc90c91684a06158
十雪拾雪 <nineto0@163.com>
2023-01-20 21:29:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 //程序入口 初始化程序
-cfg = require("./config.js");
-jsf = require("./frame/index.js");
+var cfg = require("./config.js");
+var jsf = require("./frame/index.js");
 jsf.InitFrame(cfg.config)
 
