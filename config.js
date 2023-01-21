05a5df34e0fd561910bb618cc330be231672ec48
十雪拾雪 <nineto0@163.com>
2023-01-21 18:15:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,10 @@
 'use strict';
 
 var config = {
-	pluginPath: 'plugin',
+        debug:true,
+        pluginmode:1,
+	pluginPath: 'plugin'
 }
 module.exports = config;
+//debug true/false 调试模式
+//pluginmode 0 package模式 1 文件夹遍历引入 2混合模式
